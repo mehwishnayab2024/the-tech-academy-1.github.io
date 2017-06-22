aedc2f943a13b5308414b3d60341ae5a15dc6fbd
the-tech-academy <instructor@learncodinganywhere.com>
2017-06-22 15:56:43 -0700

    initializing repo

---
+++ js/pageThree.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    document.getElementById("pageThree").style.color = "white";
+}
---
+++ js/pageTwo.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    document.getElementById("pageTwo").style.color = "white";
+}
